3b5f2e56d7e597f71cbbc7ab0809eacb352fb31b
DevSon0812 <97348196+DevSon0812@users.noreply.github.com>
2023-07-18 15:33:23 +0700

    fix responesize

--- src/components/ProjectSingle.tsx
+++ src/components/ProjectSingle.tsx
@@ -28,7 +28,7 @@ const ProjectSingle: React.FC<IProjectSignProps> = ({
       <Link
         href={path}
         key={`experience-product-${id}`}
-        className="flex flex-col-reverse md:flex-row justify-start items-start gap-3 lg:p-4 transition-all duration-200 rounded-md cursor-pointer group backdrop-filter hover:bg-white/5 hover:backdrop-blur-sm hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)]"
+        className="flex flex-col-reverse md:flex-row justify-start items-start gap-3 lg:p-4 transition-all duration-200 rounded-md cursor-pointer group backdrop-filter lg:hover:bg-white/5 hover:backdrop-blur-sm lg:hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)]"
       >
         <div className="flex flex-col w-full md:w-40 mt-2 md:mt-0 lg:w-[30%]">
           {year && mouth ? (
@@ -48,27 +48,27 @@ const ProjectSingle: React.FC<IProjectSignProps> = ({
             />
           )}
         </div>
-        <div className="w-[70%]">
+        <div className=" w-full lg:w-[70%]">
           <div className="">
-            <div>
-              <h3 className="inline-flex items-baseline text-base font-medium leading-tight text-text-primary group-hover:text-gr-5a focus-visible:text-gr-5a ">
-                {namePr}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  className="inline-block w-4 h-4 ml-1 transition-transform translate-y-px shrink-0 group-hover:-translate-y-1 group-hover:translate-x-1 group-focus-visible:-translate-y-1 group-focus-visible:translate-x-1 motion-reduce:transition-none"
-                  aria-hidden="true"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-              </h3>
-            </div>
-            <p className="mt-3">{descriptions}</p>
+            <h3 className="inline-flex items-baseline text-base font-medium leading-tight text-text-primary lg:group-hover:text-gr-5a lg:focus-visible:text-gr-5a ">
+              {namePr}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="inline-block w-4 h-4 ml-1 transition-transform translate-y-px shrink-0 lg:group-hover:-translate-y-1 lg:group-hover:translate-x-1 lg:group-focus-visible:-translate-y-1 lg:group-focus-visible:translate-x-1 motion-reduce:transition-none"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </h3>
+            <p className="mt-3 text-sm font-medium lg:text-base">
+              {descriptions}
+            </p>
           </div>
           {tech && (
             <ul className="flex flex-wrap items-center gap-2 mt-5">
